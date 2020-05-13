baf7d34f949bc42ea93f2fdc40f03bd7748b02f7
Fabian Vieri <vierifabian@gmail.com>
2020-05-13 15:37:13 +0700

    Updates

---
+++ precache-manifest.da1ee759cc58c5a32ce156e7a5216dd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e366581b77a8b68bd1dab12b521f98d8",
+    "url": "/decify/index.html"
+  },
+  {
+    "revision": "c233014118ff3a4e3326",
+    "url": "/decify/static/css/2.f28fb150.chunk.css"
+  },
+  {
+    "revision": "6333c66894de358e14f6",
+    "url": "/decify/static/css/main.650183ff.chunk.css"
+  },
+  {
+    "revision": "c233014118ff3a4e3326",
+    "url": "/decify/static/js/2.80ca2376.chunk.js"
+  },
+  {
+    "revision": "e411a53168e72d807d2c24409461a622",
+    "url": "/decify/static/js/2.80ca2376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6333c66894de358e14f6",
+    "url": "/decify/static/js/main.f0c53576.chunk.js"
+  },
+  {
+    "revision": "5f523fce5733f2c2bac8",
+    "url": "/decify/static/js/runtime-main.2427ca8d.js"
+  },
+  {
+    "revision": "57439860e52dfe06daba25b85594726e",
+    "url": "/decify/static/media/logo.57439860.png"
+  }
+]);
